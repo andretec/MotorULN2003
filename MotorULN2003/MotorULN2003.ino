0e19fe306b725298cecd54e90f34b1d33a0ccad2
AndreTec <andretec@ifsc.usp.br>
2020-07-03 11:09:19 -0300

    add function move_motor and two kinds of step movements

--- MotorULN2003/MotorULN2003.ino
+++ MotorULN2003/MotorULN2003.ino
@@ -4,15 +4,21 @@
   
 */
 
-unsigned int motor1 = 8;
-unsigned int motor2 = 8;
-unsigned int motor3 = 8;
+byte full_step[4] = {0x09,0x03,0x06,0x0C}; // high torque, wave(1)
+byte wave_step[4] = {0x01,0x02,0x04,0x08}; // low torque, wave(2)
 
-unsigned int motor1_d = 0; // Directions
-unsigned int motor2_d = 0;
-unsigned int motor3_d = 0;
+byte motor1 = 1; // Variables
+byte motor2 = 1;
+byte motor3 = 1;
 
-// the setup function runs once when you press reset or power the board
+bool motor1_d = false; // Directions
+bool motor2_d = false;
+bool motor3_d = false;
+
+unsigned int delayTime = 3; // Time between steps; 2 <- min value
+
+unsigned long time1,time2;
+
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
@@ -29,57 +35,78 @@ void setup() {
   // set pins 0..3 as output (1)
   DDRC = B00001111;  // digital pins A0,A1,A2,A3
 
-  PORTB = 1;
-  PORTC = 1;
-  PORTD = 1;
-                    
+  PORTB = 0; // Turn off motors
+  PORTC = 0;
+  PORTD = 0;
+                 
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  
-  // Motor1
-  // --------------------------------
-  PORTD = motor1 * 16;
-  if (motor1_d == 0) {
-    motor1 = motor1 >> 1;
-  }else{
-    motor1 = motor1 << 1;
-  }
-  if (motor1 > 8) { motor1 = 1; }
-  if (motor1 < 1) { motor1 = 8; }
-  // --------------------------------
 
-  // Motor2
-  // --------------------------------
-  PORTB = motor2;
-  if (motor2_d == 0) {
-    motor2 = motor2 >> 1;
-  }else{
-    motor2 = motor2 << 1;
-  }
-  if (motor2 > 8) { motor2 = 1; }
-  if (motor2 < 1) { motor2 = 8; }
-  // --------------------------------
+  //time1 = millis();
 
-  // Motor3
-  // --------------------------------
-  PORTC = motor3;
-  if (motor3_d == 0) {
-    motor3 = motor3 >> 1;
-  }else{
-    motor3 = motor3 << 1;
-  }
-  if (motor3 > 8) { motor3 = 1; }
-  if (motor3 < 1) { motor3 = 8; }
-  // --------------------------------
-  
-  
+
+  //time2 = millis();
+  //Serial.println(time2-time1);
   //delay(1);
   //PORTD = B00000000; // sets digital pins 7,5,3 HIGH
   //delay(10);
 
-  delay(500);
+  move_motor(1,2048,false,2);
+
+  delay(5000);
   //Serial.println("Hello !!!");
   
+}
+
+void move_motor(byte motor,unsigned int n_steps,bool directions,byte kind_wave){
+  
+  for (int i = 0; i <= n_steps; i++) {
+    if (motor == 1) {  
+      if (motor1 > 4) { motor1 = 1; }
+      if (motor1 < 1) { motor1 = 4; } 
+      if (kind_wave == 1){
+        PORTD = wave_step[motor1-1] * 16; // Write data in the Port
+      }else{
+        PORTD = full_step[motor1-1] * 16; // Write data in the Port
+      }
+      if (directions) {
+        motor1 = motor1 + 1;
+      }else{
+        motor1 = motor1 - 1;
+      }
+    }
+
+    if (motor == 2) {  
+      if (motor2 > 4) { motor2 = 1; }
+      if (motor2 < 1) { motor2 = 4; } 
+      PORTB = wave_step[motor2-1]; // Write data in the Port
+      if (directions) {
+        motor2 = motor2 + 1;
+      }else{
+        motor2 = motor2 - 1;
+      }
+    }
+
+    if (motor == 3) {  
+      if (motor3 > 4) { motor3 = 1; }
+      if (motor3 < 1) { motor3 = 4; } 
+      PORTC = wave_step[motor3-1]; // Write data in the Port
+      if (directions) {
+        motor3 = motor3 + 1;
+      }else{
+        motor3 = motor3 - 1;
+      }
+    }
+    
+    delay(delayTime); // Time between steps
+    
+  } // For loop
+
+    // Turn off motor
+    if (motor == 1) { PORTD = 0; }
+    if (motor == 2) { PORTB = 0; }
+    if (motor == 3) { PORTC = 0; }
+    
 }
