9889f5370b5ec04014ee1c6bd483a26ec0397ae9
AndreTec <andretec@ifsc.usp.br>
2020-07-14 11:12:07 -0300

    update move_motor Routine

--- MotorULN2003/MotorULN2003.ino
+++ MotorULN2003/MotorULN2003.ino
@@ -4,8 +4,8 @@
   
 */
 
-byte full_step[4] = {0x09,0x03,0x06,0x0C}; // high torque, wave(1)
-byte wave_step[4] = {0x01,0x02,0x04,0x08}; // low torque, wave(2)
+byte wave_step[4] = {0x01,0x02,0x04,0x08}; // low torque, (1)
+byte full_step[4] = {0x09,0x03,0x06,0x0C}; // high torque,(2)
 
 byte motor1 = 1; // Variables
 byte motor2 = 1;
@@ -17,7 +17,11 @@ bool motor3_d = false;
 
 unsigned int delayTime = 3; // Time between steps; 2 <- min value
 
-unsigned long time1,time2;
+int Str;          // Receive instructions from USB
+String sdata="";  // Used to concatenate the numbers
+
+bool LoopUSB = true;
+
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -44,62 +48,111 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
 
-  //time1 = millis();
 
+  while (Serial.available() > 0 ) {
 
-  //time2 = millis();
-  //Serial.println(time2-time1);
-  //delay(1);
-  //PORTD = B00000000; // sets digital pins 7,5,3 HIGH
-  //delay(10);
+    Str = Serial.read();
 
-  move_motor(1,2048,false,2);
+    // "M" move motor 
+    if (Str == 'M' ) {
 
-  delay(5000);
-  //Serial.println("Hello !!!");
+        LoopUSB = true;
+        Serial.println("OK");
+        delay(10);
+        
+        byte ch = "";
+        // ----------------------------------------------------
+        while ( LoopUSB ) {
+            if ( Serial.available() > 0 ){
+              ch = Serial.read();
+              if (ch=='E') {
+                Serial.print("valor = ");
+                Serial.println(sdata);
+                
+                LoopUSB = false;
+                unsigned int valor = sdata.toInt();
+                Serial.println(valor+1);
+
+                sdata = ""; // Clear the string ready for the next command.
+              }
+              else
+              {
+                Serial.println(ch);
+                sdata += (char)ch;
+              }  
+            } // if
+
+            delay(10);
+         } // while ( LoopUSB )
+         // ----------------------------------------------------
+      } // if (Str.equals("move"))
+  } // while (Serial.available() > 0 )
+  
+
+  
+  move_motor(1,2048,true,2); // high torque,(2)
+
+  //delay(1000);
+  Serial.println("Hello !!!");
   
 }
 
+// Move motor routine
 void move_motor(byte motor,unsigned int n_steps,bool directions,byte kind_wave){
   
   for (int i = 0; i <= n_steps; i++) {
+    // -------------------------------------------------------- Motor #1  
     if (motor == 1) {  
       if (motor1 > 4) { motor1 = 1; }
       if (motor1 < 1) { motor1 = 4; } 
+      // ---------------------------
       if (kind_wave == 1){
         PORTD = wave_step[motor1-1] * 16; // Write data in the Port
       }else{
         PORTD = full_step[motor1-1] * 16; // Write data in the Port
       }
+      // ---------------------------
       if (directions) {
         motor1 = motor1 + 1;
       }else{
         motor1 = motor1 - 1;
       }
     }
-
+    // -------------------------------------------------------- Motor #2
     if (motor == 2) {  
       if (motor2 > 4) { motor2 = 1; }
       if (motor2 < 1) { motor2 = 4; } 
-      PORTB = wave_step[motor2-1]; // Write data in the Port
+      // ---------------------------
+      if (kind_wave == 1){
+        PORTB = wave_step[motor2-1]; // Write data in the Port
+      }else{
+        PORTB = full_step[motor2-1]; // Write data in the Port
+      }
+      // --------------------------- 
       if (directions) {
         motor2 = motor2 + 1;
       }else{
         motor2 = motor2 - 1;
       }
     }
-
+    // -------------------------------------------------------- Motor #3
     if (motor == 3) {  
       if (motor3 > 4) { motor3 = 1; }
       if (motor3 < 1) { motor3 = 4; } 
-      PORTC = wave_step[motor3-1]; // Write data in the Port
+      // ---------------------------
+      if (kind_wave == 1){
+        PORTC = wave_step[motor3-1]; // Write data in the Port
+      }else{
+        PORTC = full_step[motor3-1]; // Write data in the Port
+      }
+      // ---------------------------
       if (directions) {
         motor3 = motor3 + 1;
       }else{
         motor3 = motor3 - 1;
       }
     }
-    
+    // --------------------------------------------------------
     delay(delayTime); // Time between steps
     
   } // For loop
